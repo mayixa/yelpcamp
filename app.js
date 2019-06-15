3f885a4660353d73331afc4e2b94f213e0c6353b
Emma <emmaborghult@gmail.com>
2019-06-15 22:48:25 +0200

    Adjust nav + add bootstrap JS + more styling

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const campgrounds = [
     {name: "Åsen", image: "https://cdn.pixabay.com/photo/2015/08/27/00/23/finland-909266_1280.jpg"},
     {name: "Vreken", image: "https://cdn.pixabay.com/photo/2017/06/26/21/02/camp-2445212_1280.jpg"},
     {name: "Klohill", image: "https://cdn.pixabay.com/photo/2017/02/14/08/51/wintry-2065342_1280.jpg"},
+    {name: "Stubben", image: "https://cdn.pixabay.com/photo/2015/10/12/14/58/camping-984038_1280.jpg"},
+    {name: "Klohill", image: "https://cdn.pixabay.com/photo/2017/02/14/08/51/wintry-2065342_1280.jpg"},
     {name: "Stubben", image: "https://cdn.pixabay.com/photo/2015/10/12/14/58/camping-984038_1280.jpg"}
 ]
 
